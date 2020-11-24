0cb44431bae107fc404de5973fc2b9743a6d3b8c
Ayoamaro <ayoze.amaro@gmail.com>
2020-11-24 21:35:10 +0000

    Fix files problems

--- src/main/java/dad/javafx/email/Controller.java
+++ src/main/java/dad/javafx/email/Controller.java
@@ -4,16 +4,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 /**
  * @author Ayoze Amaro
@@ -21,10 +26,10 @@ import javafx.scene.layout.GridPane;
  */
 public class Controller implements Initializable {
 
-	// Model
+	// MODEL
 	private Model modelo = new Model();
 	
-	// View
+	// VIEW
 	@FXML
 	private GridPane view;
 	@FXML
@@ -45,11 +50,52 @@ public class Controller implements Initializable {
 	}
 	
 	@FXML
-	void onEnviarButtonAction (ActionEvent e) { }
+	void onEnviarButtonAction (ActionEvent e) { 
+		try {
+			modelo.getEmail().setHostName(servidorText.getText());
+			modelo.getEmail().setSmtpPort(Integer.parseInt(puertoText.getText()));
+			modelo.getEmail().setAuthentication(remitenteText.getText(), remitentePassword.getText());
+			modelo.getEmail().setSSLOnConnect(sslCheckBox.isSelected());
+			modelo.getEmail().setFrom(remitenteText.getText());
+			modelo.getEmail().setSubject(asuntoText.getText());
+			modelo.getEmail().setMsg(mensajeText.getText());
+			modelo.getEmail().addTo(destinatarioText.getText());
+			
+			modelo.getEmail().send();
+			successAccess();
+			} catch(Exception ex) {
+				errorAccess();
+			}
+	}
+	
+	private void successAccess() {
+		Alert alert=new Alert(AlertType.INFORMATION);
+		alert.setHeaderText("Mensaje enviado con éxito a '"+ destinatarioText.textProperty().get() +"'.");
+		alert.setTitle("Mensaje enviado");
+		alert.showAndWait();
+	}
+	
+	private void errorAccess() {
+		Alert alert=new Alert(AlertType.ERROR);
+		alert.setHeaderText("No se pudo enviar el email");
+		alert.setTitle("Error");
+		alert.showAndWait();
+	}
+	
 	@FXML
-	void onVaciarButtonAction (ActionEvent e) { }
+	void onVaciarButtonAction (ActionEvent e) {
+		servidorText.clear();
+		puertoText.clear();
+		remitenteText.clear();
+		remitentePassword.clear();
+		sslCheckBox.setSelected(false);
+		mensajeText.clear();
+		destinatarioText.clear();
+		asuntoText.clear();
+	}
+	
 	@FXML
-	void onCerrarButtonAction (ActionEvent e) { }
+	void onCerrarButtonAction (ActionEvent e) { Platform.exit(); }
 	
 	public void initialize(URL location, ResourceBundle resources) { }
 	
--- src/main/java/dad/javafx/email/Model.java
+++ src/main/java/dad/javafx/email/Model.java
@@ -1,9 +1,20 @@
 package dad.javafx.email;
 
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+
 /**
  * @author Ayoze Amaro
  *
  */
 public class Model {
 
+	private Email email = new SimpleEmail();
+
+	public Email getEmail() {
+		return email;
+	}
+	public void setEmail(Email email) {
+		this.email = email;
+	}
 }
